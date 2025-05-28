4493a8d91d61f4b027e0e093b1a2b2032cec28b3
thecatthatflies.dev <aiyan041c10@outlook.com.au>
2025-05-28 07:53:04 -0400

    Update bingEmbed.js

--- bingEmbed.js
+++ bingEmbed.js
@@ -2,4 +2,4 @@
 //and will refuse connection. Html games work, no flash. 
 //Krunker.io works.
 //Change the url to a proxy if you want more functionality. 
-javascript:document.body.innerHTML = "<iframe src=\"https:\/\/bing.com\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
+javascript:document.body.innerHTML = "<iframe src="Google.com" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>";
